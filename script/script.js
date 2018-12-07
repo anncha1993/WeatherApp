b31ee29c5095cb342f1f24256b5c36c5ed93b383
anncha1993 <anncharlottkarlsen00@gmail.com>
2018-12-08 00:45:50 +0100

    added weather to the cities

--- script/script.js
+++ script/script.js
@@ -10,29 +10,36 @@ function update(){
 	xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/group?id=2643743,6453366,764679&APPID=c6331badce9c725318e21ae37054570f&units=metric", true);
 	xmlhttp.send();
 
-
-	
-
-
 }
-
 
 function statechange(){
 	if (xmlhttp.readyState === 4 && xmlhttp.status ===200) {
 		var info = JSON.parse(xmlhttp.responseText);
 		for (var i = 0; i < info.list.length; i++) {
-			var london = "<div class='london'>" + info.list[0].name; + "</div>"; 
+			var london = "<div class='city'>" + info.list[0].name + "</div>";
+			var vaerLondon = "<div class='weather'>" + info.list[0].weather[0].main + "</div>"; 
 			var oslo = "<div class='oslo'>" + info.list[1].name; + "</div>"; 
+			var vaerOslo = "<div class='weather'>" + info.list[1].weather[0].main + "</div>"; 
 			var minsk = "<div class='minsk'>" + info.list[2].name; + "</div>"; 
+			var vaerMinsk = "<div class='weather'>" + info.list[2].weather[0].main + "</div>"; 
+		
+		
+			
+
+
+
+
+			
+		
 
 			console.log(JSON.stringify(info.list[1]));
 		}
 		
-		document.getElementById("data").innerHTML = london + oslo + minsk;
+		document.getElementById("london").innerHTML = london + vaerLondon;
+		document.getElementById("oslo").innerHTML = oslo + vaerOslo;
+		document.getElementById("minsk").innerHTML = minsk + vaerMinsk;
 		
 	}
 }
 
 
-		//	var display = "<div class='prog'> City: "+ JSON.stringify(info.name) + "</br> Weather: " 
-		//	+  JSON.stringify(info.weather[i].main) + "</br> <img src='"+ JSON.stringify(info.weather.icon) + ".png'";
