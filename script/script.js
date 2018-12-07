04d0a87ecbdb9cfe08c463b30eb6d374b0d9e27a
anncha1993 <anncharlottkarlsen00@gmail.com>
2018-12-07 22:51:21 +0100

    testing writing out data from API

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
 window.onload = update;
 
+var xmlhttp;
+
 
 function update(){
 
-	
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = statechange;
+	xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c6331badce9c725318e21ae37054570f&units=metric", true);
+	xmlhttp.send();
+
+
 }
+
+
+function statechange(){
+
+	if (xmlhttp.readyState === 4 && xmlhttp.status ===200) {
+		document.getElementById("data").innerHTML = xmlhttp.responseText;
+	}
+}
+
+
