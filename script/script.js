f0e0d1706e3b9d689c2c6781c291196cde0130da
anncha1993 <anncharlottkarlsen00@gmail.com>
2018-12-08 02:40:11 +0100

    clean up

--- script/script.js
+++ script/script.js
@@ -15,24 +15,52 @@ function statechange(){
 	if (xmlhttp.readyState === 4 && xmlhttp.status ===200) {
 		var info = JSON.parse(xmlhttp.responseText);
 		for (var i = 0; i < info.list.length; i++) {
-			var london = "<ul> <li class='city'>" + info.list[0].name + "</li>" + "<li class='weather'>" + info.list[0].weather[0].main + " </li>"
-			+ "<li><img src='http://openweathermap.org/img/w/" + info.list[0].weather[0].icon + ".png'/></li>" + "<li class='temp'>" + info.list[0].main.temp +"C" + "</li> </ul>";
 			
-			var oslo = "<ul> <li class='oslo'>" + info.list[1].name; + "</li>" + "<li class='weather'>" + info.list[1].weather[0].main + "</li>" + "<li><img src='http://openweathermap.org/img/w/" + info.list[1].weather[0].icon + ".png'/> </li>" 
-			+ "<li class='temp'>" + info.list[1].main.temp +"C" + "</li></ul>" ; 
-		
+			var london = info.list[0].name;
+			var vaerLondon = info.list[0].weather[0].main; 
+			var iconLondon = "<img src='http://openweathermap.org/img/w/" + info.list[0].weather[0].icon + ".png'/>"; 
+			var tempLondon = info.list[0].main.temp +"C";
 			
-	
+			var wholeLondon = 
+			"<ul>" + 
+				"<li>"+london+"</li>" +
+				"<li>" + vaerLondon +"</li>" + 
+				"<li>" + iconLondon +"</li>" + 
+				"<li>" + tempLondon +"</li>" + 
+			"</ul>";
+
 			
-			var minsk = "<li class='minsk'>" + info.list[2].name; + "</li>" + "<li class='weather'>" + info.list[2].weather[0].main + "</li>" + "<li><img src='http://openweathermap.org/img/w/" 
-			+ info.list[2].weather[0].icon + ".png'/></li>" +"<li class='temp'>" + info.list[2].main.temp +"C" + "</li> </ul>"; 
-	
+			var oslo = info.list[1].name;
+			var vaerOslo = info.list[1].weather[0].main; 
+			var iconOslo = "<img src='http://openweathermap.org/img/w/" + info.list[1].weather[0].icon + ".png'/>";
+			var tempOslo = info.list[1].main.temp +"C"; 
+
+			var wholeOslo = 
+			"<ul>" + 
+				"<li>"+oslo+"</li>" +
+				"<li>" + vaerOslo +"</li>" + 
+				"<li>" + iconOslo +"</li>" + 
+				"<li>" + tempOslo +"</li>" + 
+			"</ul>";
+			
+			var minsk = info.list[2].name; 
+			var vaerMinsk = info.list[2].weather[0].main; 
+			var iconMinsk = "<img src='http://openweathermap.org/img/w/" + info.list[2].weather[0].icon + ".png'/>";
+			var tempMinsk = info.list[2].main.temp +"C"; 
+
+			var wholeMinsk = 
+			"<ul>" + 
+				"<li>"+minsk+"</li>" +
+				"<li>" + vaerMinsk +"</li>" + 
+				"<li>" + iconMinsk +"</li>" + 
+				"<li>" + tempMinsk +"</li>" + 
+			"</ul>"; 
 		
-		
+			console.log(JSON.stringify(info.list[1]));
 		}
-		document.getElementById("london").innerHTML = london;
-		document.getElementById("oslo").innerHTML = oslo;
-		document.getElementById("minsk").innerHTML = minsk;	
+		document.getElementById("london").innerHTML = wholeLondon;
+		document.getElementById("oslo").innerHTML = wholeOslo;
+		document.getElementById("minsk").innerHTML = wholeMinsk;	
 	}
 }
 
